44c36c32adbde20df8e83ea2ec506bf46e1cbbd7
CosmicGames101 <98639738+CosmicGames101@users.noreply.github.com>
2024-03-10 06:55:41 -0500

    fixed code hopefully

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Array of failure messages
 var failureMessages = [
     "Incorrect. Please quit now.",
-    "That's not quite right. Maybe quit while your ahead?",
+    "That's not quite right. Maybe quit while you're ahead?",
     "Oops, wrong answer. This means you're a noble member!",
     "Not quite. Give up fodder!",
-    "Incorrect answer. You're close! just kidding you are a noble phantasm member! Sucks to suck"
+    "Incorrect answer. You're close! Just kidding, you are a noble phantasm member! Sucks to suck."
 ];
 
 // Function to check the user's answer
@@ -12,8 +12,8 @@ function checkAnswer(hint) {
     var answer = document.getElementById("answer").value.toLowerCase();
     var result = document.getElementById("result");
 
-    if (answer === "elephant") {
-        result.textContent = "Correct! this must mean you aren't a noble member meaning no brainrot. Good luck with the riddle!";
+    if (answer === "if anyone is able to decipher this text then you have found the first hint, the first hint is zxclosure") {
+        result.textContent = "Correct! This must mean you aren't a noble member meaning no brainrot. Good luck with the riddle!";
         // Add logic here to navigate to the next riddle
     } else {
         // Randomly select a failure message
